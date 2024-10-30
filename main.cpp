24be8fddd9239d472266e0a3dff9a674f02933b8
Aleksandra <azakrecka@student.agh.edu.pl>
2024-10-30 21:57:52 +0100

    symulacja

--- main.cpp
+++ main.cpp
@@ -87,7 +87,7 @@ void lab1()
 		cout << res[0] << endl << res[1] << endl << solution::f_calls << endl << endl;
 		//Sout << "x" << res[0] << ";" << "x" << res[1] << ";" << "x" << solution::f_calls << "\n";
 
-		wynik = fib(ff1T, res[0], res[1], epsilon);
+		wynik = fib(ff1T, -100, 100, epsilon);
 		//Sout << "x" << wynik.x << "x" << wynik.y << "x" << wynik.f_calls << "\n";
 		cout << wynik << endl;
 
@@ -98,24 +98,62 @@ void lab1()
 		x0 = x0 + 2;
 	}
 
-
-
 	//zadanie praktyczne
 
-	//double* res = new double[2] { 0, 0 };
-	//double da = 0.005;
-	//double delta_da = 0.002;
-	//double tmax = 0;
-	//double alpha = 1.5;
-	//double epsilon = 0.0001;
-	//double gamma = 0.000001;
-	//int nmax = 1000;
+	double* res = new double[2] { 0, 0 };
+	double da = 0.005, delta_da = 0.002;
+	double alpha = 1.5, epsilon = 0.0001, gamma = 0.000001;
+	int nmax = 1000;
 
-	//res = expansion(ff2T, da, delta_da, alpha, nmax);
-	//solution wynik;
-	////wynik = fib(ff2T, res[0], res[1], epsilon); 
-	//wynik = lag(ff2T, res[0], res[1], epsilon, gamma, nmax);
+	res = expansion(ff2T, da, delta_da, alpha, nmax);
+	solution wynik;
+	//wynik = fib(ff2T, res[0], res[1], epsilon); 
+	cout << "Metoda Fibonacciego: " << endl;
+	cout << "Optymalna wielosc otwou D_A: " << wynik.x << "\nMaksymalna temperatura wody w zbiorniku: " << wynik.y + 50 << "|" << wynik.y << "\nLiczna wywolan fukcji: " << wynik.f_calls << "\nExit flag: " << wynik.flag << endl;;
 	//cout << wynik;
+	wynik = lag(ff2T, res[0], res[1], epsilon, gamma, nmax);
+	cout << "Metoda Lagrangea: " << endl;
+	cout << "Optymalna wielosc otworu D_A: " << wynik.x << "\nMaksymalna temperatura wody w zbiorniku: " << wynik.y + 50 << "|" << wynik.y << "\nLiczna wywolan fukcji: " << wynik.f_calls << "\nExit flag: " << wynik.flag << endl;
+	//cout << wynik;
+
+	//symlacja 
+
+	// Warunki początkowe
+	matrix Y0(3, 1);
+	Y0(0) = 5.0;   // Początkowa objętość w zbiorniku A (VA)
+	Y0(1) = 1.0;   // Początkowa objętość w zbiorniku B (VB)
+	Y0(2) = 20.0;  // Początkowa temperatura w zbiorniku B (TB)
+
+	// Czas symulacji
+	double t0 = 0.0;            // Początkowy czas
+	double tend = 2000.0;       // Końcowy czas symulacji
+	double dt = 1.0;            // Krok czasowy
+
+	matrix ud1(1, 1), ud2;
+	ud1(0) = m2d(wynik.x);
+	
+	matrix* S = solve_ode(df1, t0, dt, tend, Y0, ud1, ud2);
+
+	int N = static_cast<int>(floor((tend - t0) / dt) + 1);
+	std::vector<double> t_values(N);
+	std::vector<double> VA_values(N);
+	std::vector<double> VB_values(N);
+	std::vector<double> TB_values(N);
+
+	for (int i = 0; i < N; ++i) {
+		t_values[i] = S[0](i);
+		VA_values[i] = S[1](i, 0); // Pierwsza kolumna to VA
+		VB_values[i] = S[1](i, 1); // Druga kolumna to VB
+		TB_values[i] = S[1](i, 2); // Trzecia kolumna to TB
+	}
+
+	std::ofstream file("symulacja_lab1.csv");
+	file << "Czas;VA;VB;TB\n";
+	for (int i = 0; i < N; ++i) {
+		file << t_values[i] << "x;" << VA_values[i] << "x;" << VB_values[i] << "x;" << TB_values[i] << "x\n";
+	}
+	file.close();
+
 }
 
 void lab2()
